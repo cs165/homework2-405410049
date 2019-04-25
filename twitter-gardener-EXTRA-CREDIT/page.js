10735f9b893567a95c49b9499c31dbd586147ce8
405410049 <as851468567@yahoo.com.tw>
2019-04-26 01:13:50 +0800

    Add files via upload

--- twitter-gardener-EXTRA-CREDIT/page.js
+++ twitter-gardener-EXTRA-CREDIT/page.js
@@ -17,13 +17,63 @@ const POSITIVE_MESSAGES = [
 chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(onMessage);
 });
-
-function onMessage(gardeningInProgress) {
-  // TODO(you): Implement this function for extra credit! Add helper functions
-  // as needed.
-
-  // NOTE: This extension is EXTRA CREDIT and is not required for HW2.
-
+if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="/extension_style.css">');
+function onMessage(gardeningInProgress,event) {
   // If `gardeningInProgress` is true, that means "Start Gardening" was clicked.
   // If `gardeningInProgress` is false, that means "Stop Gardening" was clicked.
+ // var select=document.querySelectorAll('.stream div');
+ var select=document.querySelectorAll('.tweet');
+  if(gardeningInProgress==true)
+  {
+	  const mouse_url = chrome.runtime.getURL('images/rose-cursor.gif');
+	  for(let obj of select)
+	  {
+		obj.style.cursor = 'url(' + mouse_url + ') 4 12, auto';
+		obj.addEventListener('mouseover',function(){over(obj)});
+		obj.addEventListener('mouseout',function(){out(obj)});
+		obj.addEventListener('click',function(e){onClick(e,obj)});
+	  }
+  }
+  else
+  {
+	select=document.querySelectorAll('.tweet');  
+	console.log("123");
+	for(let obj of select)
+	{
+		obj.style.cursor = '';
+		//obj.opacity='';
+		obj.removeEventListener('mouseover',over(obj));
+		obj.removeEventListener('mouseover',function(){over()});
+		//obj.removeEventListener('mouseover',out);
+	}
+  }
+
+}
+function over(obj)
+{
+	const img_url=chrome.runtime.getURL('images/sparkle.gif');
+	obj.style.opacity=0.8;
+	obj.style.backgroudImage='url('+img_url+')';
+}
+function out(obj)
+{
+	obj.style.opacity=1;
+	obj.style.backgroudImage='';
+}
+function onClick(e,obj)
+{
+	//event.stopPropagation();
+	e.stopPropagation();
+	obj1=obj.querySelector('.js-tweet-text-container');
+	obj1.textContent=POSITIVE_MESSAGES[Math.floor(Math.random()*POSITIVE_MESSAGES.length)];
+	obj2=obj.querySelector('.QuoteTweet-text');
+	if(obj2!=null)
+		obj2.textContent=POSITIVE_MESSAGES[Math.floor(Math.random()*POSITIVE_MESSAGES.length)];
+}
+ var  select=document.querySelectorAll('.stream div');
+for(let x of select)
+	x.addEventListener('click',checkTarget);
+function checkTarget(event)
+{
+	console.log(event.target);
 }
