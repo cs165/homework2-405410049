121b8ffb899581137ef95f317e40a67b50f16443
405410049 <as851468567@yahoo.com.tw>
2019-04-26 01:13:20 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -1,4 +1,5 @@
-const MATCH_LIST = {
+function transformTextNodes(node) {
+	const MATCH_LIST = {
   'there': 'their',
   'their': 'there',
   'they\'re': 'there',
@@ -9,12 +10,33 @@ const MATCH_LIST = {
   'THEIR': 'THERE',
   'THEY\'RE': 'THERE'
 };
-
-function transformTextNodes(node) {
-  // TODO(you): Implement this function! See HW spec for details.
+  var treeWalker = document.createTreeWalker(node,NodeFilter.SHOW_TEXT);
+  //for(let x in MATCH_LIST)
+		// console.log(x);
+  while(treeWalker.nextNode())
+  {
+	let _arr,arr,searchId,matchPattern;
+	let content=treeWalker.currentNode.textContent;
+	content=content.trim();
+	arr=content.split(' ');
+	console.log(arr);
+	for(let j=0;j<arr.length;j++)	
+		for(let i in MATCH_LIST)					
+			if(arr[j]==i)
+			{
+				console.log("!23");
+				arr[j]=MATCH_LIST[i];
+				break;
+			}
+	arr[arr.length]=' ';
+	arr=arr.join(' ');
+	treeWalker.currentNode.textContent=arr;
+	console.log(arr);
+  }
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
